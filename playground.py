a1250d960deb1acaa24a274e32240e25f4dc6ee7
Shruti <52789421+ShrutiSara@users.noreply.github.com>
2022-02-01 17:33:15 +0530

    playground

---
+++ playground.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+openai.api_key = os.environ.get('OPENAI_KEY')
+completion = openai.Completion()
+
+start_chat_log = "Human: How you doin AI: Peachy keen"
+
+def ask(question, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return answer
+
+question = "Does Harry Potter die?"
+chat_log = None
+
+while(question != "exit"):
+    question = input()
+    answer = ask(question)
+    print(answer)
+    
+
